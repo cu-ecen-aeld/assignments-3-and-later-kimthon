c5032aadcd06e88a90c7b9218f4a8f95d568c40c
kimthon <xoghks9793@gmail.com>
2025-01-19 19:25:46 +0900

    feat : Add Circular Buffer

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -29,9 +29,22 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
+    size_t cur_pos = 0;
+
+    for(int i = 0; i < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; ++i)
+    {
+        size_t pos = (i + buffer->in_offs) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+
+        if(!buffer->full && pos == buffer->out_offs) return NULL;
+
+        if(char_offset < cur_pos + buffer->entry[pos].size) 
+        {
+            *entry_offset_byte_rtn = char_offset - cur_pos;
+            return &(buffer->entry[pos]);
+        }
+        cur_pos += buffer->entry[pos].size;
+    }
+
     return NULL;
 }
 
@@ -44,9 +57,18 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description
-    */
+    buffer->entry[buffer->in_offs] = *add_entry;
+
+    buffer->in_offs = (buffer->in_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+
+    if (buffer->full)
+    {
+        buffer->out_offs = buffer->in_offs;
+    }
+    else if (buffer->in_offs == buffer->out_offs)
+    {
+        buffer->full = true;
+    }
 }
 
 /**
